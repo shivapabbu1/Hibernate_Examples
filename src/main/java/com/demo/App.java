ecebdac533ee54ab7bc99da97f7056762b9a1be5
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2024-05-31 00:28:33 +0530

    Simple Maven Hibernate Project Used Config.xml

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import java.util.List;
+
+
+import com.demo.DAO.StudentDAO;
+import com.demo.entity.Student;
+
+public class App {
+   
+    	public static void main(String[] args) {
+    		StudentDAO studentDao = new StudentDAO();
+    		Student student = new Student("shiva", "pabbu", "shivapabbu@net.com");
+    		studentDao.saveStudent(student);
+    		
+    		List<Student> students = studentDao.getStudents();
+    		students.forEach(s -> System.out.println(s.getEmail()));
+    	}
+}
