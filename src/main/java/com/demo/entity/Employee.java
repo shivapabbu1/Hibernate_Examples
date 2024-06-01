3a72372d65565ddb8d7eb4f189ad91c398725ebf
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2024-06-01 14:01:58 +0530

    Inheritence_Table_per_class

--- src/main/java/com/demo/entity/Employee.java
+++ src/main/java/com/demo/entity/Employee.java
@@ -12,17 +12,16 @@ import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "employee_type")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Employee {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
 
+
     // Getters and setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+   
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 }
