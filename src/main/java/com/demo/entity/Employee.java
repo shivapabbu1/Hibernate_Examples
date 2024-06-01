9b47fe8d4f8316ba0f934425ed6c758fe48dd31e
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2024-06-01 14:29:03 +0530

    Updated

--- src/main/java/com/demo/entity/Employee.java
+++ src/main/java/com/demo/entity/Employee.java
@@ -12,17 +12,17 @@ import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "employee_type")
 public abstract class Employee {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
-
     // Getters and setters
-   
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 }
-
--- src/main/java/com/demo/entity/FullTimeEmployee.java
+++ src/main/java/com/demo/entity/FullTimeEmployee.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("FULL_TIME")
-
 public class FullTimeEmployee extends Employee {
     private double salary;
 
