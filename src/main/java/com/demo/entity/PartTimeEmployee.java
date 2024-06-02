d3188e150a4a2b9f9a58c38591a87f324db837b5
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2024-06-02 11:19:26 +0530

    updated code

--- src/main/java/com/demo/entity/PartTimeEmployee.java
+++ src/main/java/com/demo/entity/PartTimeEmployee.java
@@ -4,7 +4,7 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("PART_TIME")
+
 public class PartTimeEmployee extends Employee {
     private double hourlyRate;
 
