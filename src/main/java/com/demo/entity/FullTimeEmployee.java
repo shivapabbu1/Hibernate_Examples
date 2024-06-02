7ed830573e757a835a7b7ff22ba267463fd2313c
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2024-06-02 11:22:38 +0530

    updated

--- src/main/java/com/demo/entity/FullTimeEmployee.java
+++ src/main/java/com/demo/entity/FullTimeEmployee.java
@@ -4,7 +4,7 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("FULL_TIME")
+
 public class FullTimeEmployee extends Employee {
     private double salary;
 
